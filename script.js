67316994629224f4b4f3e3790302beff413b2178
codenoobies <kevindubidaam@gmail.com>
2022-04-29 13:46:36 +0700

    added modal animmation and functionality

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 const element = document.getElementsByClassName("coming-soon")
+const modal = document.getElementsByClassName("modal")[0];
 
 Array.from(element).forEach((element) => {
     console.log(element);
     element.addEventListener("click", (event) => {
         event.preventDefault();
-        document.getElementsByClassName("modal")[0].classList.add("active");
+        modal.classList.add("active");
+        setTimeout(() => {
+            modal.classList.add("visible");
+        }, 100)
     })
 })
 
-const modal = document.querySelector(".modal button");
-
 modal.addEventListener("click", (event) => {
-    if (event.target.parentElement.parentElement.classList.contains("active")) {
-        document.querySelector(".modal").classList.remove("active");
+    console.log(event.target);
+    if (event.target.innerText == "X") {
+        modal.classList.remove("visible");
+        setTimeout(() => {
+            modal.classList.remove("active");
+        }, 350);
     }
 })
